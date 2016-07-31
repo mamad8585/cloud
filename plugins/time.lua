663234159057ab56cda0bd6ab31937fefe2e8efc
Lodingbot <alimohamad0409@gmail.com>
2016-07-31 17:55:39 +0000

    Update time.lua

--- plugins/time.lua
+++ plugins/time.lua
@@ -1,15 +1,44 @@
-function run(msg, matches)
+local color = {
+"red",
+"black",
+"blue",
+"green",
+"yellow",
+"magenta",
+"Orange",
+"DarkOrange",
+"Purple",
+"Golden",
+}
+local font = {
+"mathrm",
+"mathbf",
+"mathit",
+"mathfrak",
+"mathrm",
+}
+local size = {
+"300",
+"500",
+"700",
+"800",
+}
+local function run(msg, matches)
 local url , res = http.request('http://api.gpmod.ir/time/')
-if res ~= 200 then return "No connection" end
+if res ~= 200 then return "یه مشکلی هست" end
 local jdat = json:decode(url)
-local url = "http://latex.codecogs.com/png.download?".."\\dpi{800}%20\\LARGE%20"..jdat.ENtime
+local c = color[math.random(#color)]
+local s = size[math.random(#size)]
+local f = font[math.random(#font)]
+local url = "http://latex.codecogs.com/png.latex?".."\\dpi{"..s.."}%20\\huge%20\\"..f.."{{\\color{"..c.."}"..jdat.ENtime.."}}"
 local file = download_to_file(url,'time.webp')
 send_document(get_receiver(msg) , file, ok_cb, false)
 end
  
 return {
   patterns = {
-    "^!time"
+    "^ساعت$",
+    "^[!#/]time$",
   }, 
   run = run 
 }
