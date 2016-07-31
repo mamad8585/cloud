ddbec0ec454dbd4dd6603b840a2d81f02a32517f
Lodingbot <alimohamad0409@gmail.com>
2016-07-31 20:10:54 +0000

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,10 +91,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'shield' then -- Put everything you like :)
+  if matches[1]:lower() == 'telecloud4' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /shield ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /telecloud4 ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -143,8 +143,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (shield)",
-	"^[#!/]([Ss]hield)"
+    "^[#!/]([Ss]tats) (telecloud4)",
+	"^[#!/]([Tt]elecloud4)"
     }, 
   run = run
 }
